4b5bb7e9862060b4071f3a582420f58a3a72c601
Dark star <a2479865512@163.com>
2019-05-30 18:22:48 +0800

    ACM

---
+++ 数论/费马平方和定理 求圆上整点的个数.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+long long n;
+long long ans=1;
+
+int main()
+{
+    cin>>n;
+    n=n*n;
+    long long t=n;
+    while(!(t&1)) t>>=1;
+    for(int i=3;i*i<=n && t>1;i++)
+    {
+        int p=0;
+        while(t%i==0)
+        {
+            p++;
+            t/=i;
+        }
+        if(i%4==1) ans*=(p+1);
+        else if(i%4==3 && (p&1))
+        {
+            ans=0;
+            break;
+        }
+    }
+    if(t%4==3) ans=0;
+    cout<<ans*4<<endl;
+}
